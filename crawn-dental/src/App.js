0616216f603b4da2b3ee5ef18c860d40c3e22b98
ElenaIlieva22 <elena.nikolova22@gmail.com>
2024-10-18 09:20:01 +0200

    bg

--- crawn-dental/src/App.js
+++ crawn-dental/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
+import Backgrounds from './components/backgrounds/backgrounds';
 import Header from './components/Header/Header';
-import Backgrounds from "/Users/User/Desktop/WEB-DEV/Crawn-Dental-Care/crawn-dental-care/src/components/Backgrounds/background";
+
 
 function App() {
   return (
   <div>
     <Header/> 
-    <Backgrounds/> 
+    <Backgrounds />
   </div>
   )
 }
