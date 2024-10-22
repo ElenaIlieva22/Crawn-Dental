b9c685321b9d3d0b523dd74f4cd80d2b777a88d8
ElenaIlieva22 <elena.nikolova22@gmail.com>
2024-10-22 14:18:37 +0200

    app fix

--- crawn-dental/src/App.js
+++ crawn-dental/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
+import Header from './components/Header/header';
+import Backgrounds from './components/Backgrounds/backgrounds';
 
-import Header from './components/header/header.js';
-import Backgrounds from './components/backgrounds/backgrounds.js';
 
 
 function App() {
   return (
   <div>
-      <Header />
-      <Backgrounds />
+      <Header/>
+      <Backgrounds/>
   </div>
   )
 }
